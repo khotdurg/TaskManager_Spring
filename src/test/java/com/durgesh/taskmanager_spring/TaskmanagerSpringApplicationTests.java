c662cbe61b7d34e952ad69debeedeab85ba3edbf
Durgesh <durgesgkhot274@gmail.com>
2024-04-13 18:32:15 +0530

    first API

---
+++ src/test/java/com/durgesh/taskmanager_spring/TaskmanagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durgesh.taskmanager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
